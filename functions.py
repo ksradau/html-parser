d985ee424a6a308d968c338aaccc70aa82c69078
ksradau <sushei.ekaterina@gmail.com>
2020-06-19 01:20:00 +0300

    Pymorphy2 analyzing and text cleaning

--- functions.py
+++ functions.py
@@ -2,6 +2,8 @@ import sys
 import requests
 from bs4 import BeautifulSoup
 from collections import Counter
+import pymorphy2
+import string
 
 
 def get_urls_from_file():
@@ -17,10 +19,23 @@ def get_text_from_page(url):
     return text
 
 
-def get_top_words_from_text(text, number_of_words):
-    words = text.lower().split()
+def get_clean_text(text):
+    lower_text = text.lower()
+    text_without_punc = lower_text.translate(str.maketrans('', '', string.punctuation))
+
+    clean_text = text_without_punc.replace('—', '').replace('<', '').replace('>', '').replace('–', '').replace('»', '').replace('«', '')
+    return clean_text
+
+
+def get_top_words_from_text(clean_text, number_of_words):
+    words = clean_text.split()
+
+    morph = pymorphy2.MorphAnalyzer()
+    ignore_words = {'INTJ', 'PRCL', 'CONJ', 'PREP'}
+
+    words = [morph.parse(w)[0].normal_form for w in words if morph.parse(w)[0].tag.POS not in ignore_words and w.isdigit() is not True]
     most_common_words = Counter(words).most_common(number_of_words)
-    return most_common_words #list of tuples
+    return most_common_words
 
 
 def beautiful_output_for_common_words(common_words):
--- script.py
+++ script.py
@@ -6,5 +6,6 @@ urls = get_urls_from_file()
 
 for url in urls:
     text = get_text_from_page(url)
-    most_common_words = get_top_words_from_text(text, number_of_words)
-
+    clean_text = get_clean_text(text)
+    most_common_words = get_top_words_from_text(clean_text, number_of_words)
+    print(beautiful_output_for_common_words(most_common_words))

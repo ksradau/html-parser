bcaa10923a4850ccaa264dabf40bde64ba3cbabe
ksradau <sushei.ekaterina@gmail.com>
2020-06-22 20:24:37 +0300

    Args system added, command for run changed

--- functions.py
+++ functions.py
@@ -4,16 +4,39 @@ from bs4 import BeautifulSoup
 from collections import Counter
 import pymorphy2
 import string
+import argparse
+from pathlib import Path
+
+
+def get_args_from_console():
+    p = argparse.ArgumentParser(description="Args for HTML parser")
+    p.add_argument("--file", metavar="Path to your file", dest="file_with_urls", type=str)
+    app_args = p.parse_args()
+    if not app_args.file_with_urls:
+        p.print_help()
+        sys.exit(-1)
+    return app_args
+
+
+def get_file_with_urls():
+    app_args = get_args_from_console()
+    file_with_urls = Path(app_args.file_with_urls)
+    if not file_with_urls.is_file():
+        raise InputDataError("Something wrong with your input data. It must be file that contains URLs.")
+    return file_with_urls
 
 
 def get_urls_from_file():
-    with open(sys.argv[1], 'r') as f:
+    file_with_urls = get_file_with_urls()
+    with open(file_with_urls, 'r') as f:
         list_of_urls = [line.rstrip('\n') for line in f]
     return list_of_urls
 
 
 def get_text_from_page(url):
     r = requests.get(url)
+    if r.status_code != 200:
+        raise UrlError("Something wrong with URL ", url)
     soup = BeautifulSoup(r.text, 'html.parser')
     text = soup.get_text()
     return text
@@ -22,22 +45,33 @@ def get_text_from_page(url):
 def get_clean_text(text):
     lower_text = text.lower()
     text_without_punc = lower_text.translate(str.maketrans('', '', string.punctuation))
-
-    clean_text = text_without_punc.replace('—', '').replace('<', '').replace('>', '').replace('–', '').replace('»', '').replace('«', '')
+    clean_text = text_without_punc.replace('—', '').replace('–', '').replace('»', '').replace('«', '')
     return clean_text
 
 
 def get_top_words_from_text(clean_text, number_of_words):
     words = clean_text.split()
-
     morph = pymorphy2.MorphAnalyzer()
     ignore_words = {'INTJ', 'PRCL', 'CONJ', 'PREP'}
-
-    words = [morph.parse(w)[0].normal_form for w in words if morph.parse(w)[0].tag.POS not in ignore_words and w.isdigit() is not True]
+    words = [morph.parse(w)[0].normal_form for w in words if morph.parse(w)[0].tag.POS not in ignore_words and not w.isdigit()]
     most_common_words = Counter(words).most_common(number_of_words)
-    return most_common_words
-
-
-def beautiful_output_for_common_words(common_words):
-    list_of_words = [word[0] for word in common_words]
+    list_of_words = [word[0] for word in most_common_words]
     return list_of_words
+
+
+def print_beautiful_output(list_of_words, url):
+    print("Most common for ", url )
+    print(*list_of_words)
+    print("\n~ ~ ~ ~ ~ ~")
+
+
+class ParserError(Exception):
+    pass
+
+
+class UrlError(ParserError):
+    pass
+
+
+class InputDataError(ParserError):
+    pass
--- script.py
+++ script.py
@@ -1,11 +1,31 @@
-from functions import *
+from functions import get_urls_from_file, get_text_from_page, get_clean_text, get_top_words_from_text, print_beautiful_output
+from functions import ParserError
+import sys
 
-number_of_words = 10
 
-urls = get_urls_from_file()
+NUMBER_OF_WORDS = 10
 
-for url in urls:
-    text = get_text_from_page(url)
-    clean_text = get_clean_text(text)
-    most_common_words = get_top_words_from_text(clean_text, number_of_words)
-    print(beautiful_output_for_common_words(most_common_words))
+
+def to_parse():
+    urls = get_urls_from_file()
+    for url in urls:
+        try:
+            text = get_text_from_page(url)
+            clean_text = get_clean_text(text)
+            list_of_words = get_top_words_from_text(clean_text, NUMBER_OF_WORDS)
+            print_beautiful_output(list_of_words, url)
+        except ParserError:
+            print("Something was wrong during parser's work")
+
+
+def main():
+    try:
+        to_parse()
+    except Exception:
+        return 1
+    else:
+        return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
